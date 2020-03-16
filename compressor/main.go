09ec9a8bc6005615195a5c8f25f0b1e08e1ed5ca
abuarquemf <abuarquemf@gmail.com>
2020-03-16 20:07:03 -0300

    added golang files

---
+++ compressor/main.go
@@ -0,0 +1,50 @@
+package main
+
+// #include "HuffmanHandler.h"
+import "C"
+import (
+	"compressor/redis"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type in struct {
+	File  string `json:"file"`
+	Email string `json:"email"`
+}
+
+func compressAndSend(in *in) {
+	dec, err := base64.StdEncoding.DecodeString(in.File)
+	if err != nil {
+		fmt.Println("pau decodando")
+	}
+	f, err := os.Create("shared/file")
+	if err != nil {
+	}
+	defer f.Close()
+	_, err = f.Write(dec)
+	if err != nil {
+		fmt.Println("pau escrevendo arquivo")
+	}
+	err = f.Sync()
+	if err != nil {
+		fmt.Println("pau sync arquivp")
+	}
+	C.onCompress(C.CString("shared/file"), C.CString("shared/out"))
+}
+
+func main() {
+	r := redis.New()
+	inputs := make(chan []byte)
+	r.Subscribe("compression", inputs)
+	for {
+		var in in
+		err := json.Unmarshal(<-inputs, &in)
+		if err != nil {
+			fmt.Println("failed to handle: ", err)
+		}
+		go compressAndSend(&in)
+	}
+}
